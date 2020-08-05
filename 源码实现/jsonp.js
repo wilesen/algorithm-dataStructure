a5bfc6998d5b82cd7485a3ebf205dfb2563f66e1
wentingzi <wilesen@163.com>
2020-08-05 08:45:35 +0800

    算法补充

---
+++ 源码实现/jsonp.js
@@ -0,0 +1,52 @@
+/**
+ * JSONP请求工具
+ * @param url 请求的地址
+ * @param data 请求的参数
+ * @returns {Promise<any>}
+ */
+ const request = ({url, data}) => {
+  return new Promise((resolve, reject) => {
+    // 处理传参成xx=yy&aa=bb的形式
+    const handleData = (data) => {
+      const keys = Object.keys(data)
+      const keysLen = keys.length
+      return keys.reduce((pre, cur, index) => {
+        const value = data[cur]
+        const flag = index !== keysLen - 1 ? '&' : ''
+        return `${pre}${cur}=${value}${flag}`
+      }, '')
+    }
+    // 动态创建script标签
+    const script = document.createElement('script')
+    // 接口返回的数据获取
+    window.jsonpCb = (res) => {
+      document.body.removeChild(script)
+      delete window.jsonpCb
+      resolve(res)
+    }
+    script.src = `${url}?${handleData(data)}&cb=jsonpCb`
+    document.body.appendChild(script)
+  })
+}
+// 使用方式
+request({
+  url: 'http://localhost:9871/api/jsonp',
+  data: {
+    // 传参
+    msg: 'helloJsonp'
+  }
+}).then(res => {
+  console.log(res)
+})
+
+
+
+// <script type='text/javascript'>
+//       // 后端返回直接执行的方法，相当于执行这个方法，由于后端把返回的数据放在方法的参数里，所以这里能拿到res。
+//       window.jsonpCb = function (res) {
+//         console.log(res)
+//       }
+//       </script>
+//       <script src='http://localhost:9871/api/jsonp?msg=helloJsonp&cb=jsonpCb' type='text/javascript'></script>
+
+
---
+++ 算法/数组/冒泡排序.js
@@ -0,0 +1,25 @@
+/*
+*冒泡排序
+
+如果后一个元素与前一个元素比较，如果比前一个大则交换位置，同时再第二次循环时用这个元素再继续比较，这样能保证再一次循环结束以后，最后一个元素
+一定是数组中最大的一个元素，所以内层元素可以减少一次循环
+*/ 
+
+function bubbleSort(arr) {
+	var len = arr.length;
+	for (var i = 0; i < len-1; i++) {
+		for (var j = 0; j < len - 1 - i; j++) {
+			if (arr[j] > arr[j+1]) {
+				var temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	return arr;
+}
+
+//举个数组
+myArr = [20,18,27,19,35];
+//使用函数
+bubbleSort(myArr)
