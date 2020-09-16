83bdd1807f786b0e817bc7a2154a920d2d553006
wentingzi <wilesen@163.com>
2020-09-16 08:47:22 +0800

    最长回文子串&&动态规划 规则兼职工作

--- 算法/String/43.字符串相乘.js
+++ 算法/String/43.字符串相乘.js
@@ -45,30 +45,22 @@ num1 和 num2 均不以零开头，除非是数字 0 本身。
  * @return {string}
  */
  var multiply = function(num1, num2) {
-
- 	if(num1=='0'||num2=='0') return '0';
-
- 	let l1=num1.length;
- 	let l2=num2.length;
-
- 	let sumArr= new Array(l1+l2-1).fill(0),carry=0,result='';
-
- 	for(let i=0;i<l1;i++){
- 		for(let j=0;j<l2;j++){
- 			sumArr[i+j]+=+num1[i]*+num2[j]  //转为数字
- 		}
- 	}
-
- 	r=sumArr.length;
- 	
- 	while (r--) {
- 		let remain=(sumArr[r]+carry)%10;
- 		carry=(sumArr[r]+carry)/10|0;
- 		result+=remain;
- 	}
-
- 	return result;
-
- };
+    if(num1==="0"||num2==="0")return "0";
+    const l1=num1.length,l2=num2.length;
+    let sumNumarr=new Array(l1+l2-1).fill(0),carry=0,result='';
+    for(let i =0;i<l1;i++){
+         for(let j =0;j<l2;j++){
+             sumNumarr[i+j]+=+num1[i]*+num2[j]
+        }
+    }
+    let r2=sumNumarr.length;
+    while(r2--){
+        let remain=(sumNumarr[r2]+carry)%10;
+        carry=Math.floor((sumNumarr[r2]+carry)/10);
+        console.log(remain,carry);
+        result+=remain;
+    }
+    return carry?carry+result.split('').reverse().join(''):result.split('').reverse().join('');
+};
 
  multiply('9','99')
