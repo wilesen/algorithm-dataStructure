b6e11e7b95fa721e1d499228528dcb6e660287f0
wentingzi <wilesen@163.com>
2021-04-22 19:47:15 +0800

    add：350两个数组交集2

--- 源码实现/apply.js
+++ 源码实现/apply.js
@@ -1,18 +1,18 @@
-function test(a){
+function test(a) {
 	console.log(a)
 	console.log(this.name);
 	console.log(arguments);
 }
 
-Function.prototype.myApply=function(target){
+Function.prototype.myApply = function (target) {
 	// 这里是为了保证在test函数内的this是指向target，否则的话指向就是this  ---》理解为 target.test()  this指向改为tartget
-	target._$this=this;
-	let args=arguments[1];
+	target._$this = this;
+	let args = arguments[1];
 	let result;
-	if(args){
-		result =target._$this(...args);
-	}else{
-		result =target._$this();
+	if (args) {
+		result = target._$this(...args);
+	} else {
+		result = target._$this();
 	}
 	delete target._$this;
 	return result;
@@ -20,18 +20,5 @@ Function.prototype.myApply=function(target){
 
 
 
-test.myApply({name:123},[1,2,3,4]);
+test.myApply({ name: 123 }, [1, 2, 3, 4]);
 
-
-Function.prototype.applys=(argument)=>{
-	target.this=this||window;
-	let args=arguments[1];
-	let result;
-	if(args){
-		resize = =target.this(...args);
-	}else{
-		resize=target.this();
-	}
-	delete target._$this;
-	return result;
-}
--- 源码实现/bind.js
+++ 源码实现/bind.js
@@ -1,29 +1,18 @@
-Function.prototype.myBind = function(target){
-	let context = target||window;
-	let _this=this;
-	let _args=[...arguments].splice(1);
-
-	return function (){
-		let args=arguments;
-		return _this.apply(context,[...args,_args])
+Function.prototype.myBind = function (target) {
+	let context = target || window;
+	const _this = this;
+	let _args = [...arguments].splice(1);
+	return function () {
+		_this.apply(context, [...arguments, ..._args,])
 	}
-};
+}
 
-
-function test(){
+function test() {
 	console.log(this.name)
 }
 
-test.bind({name:2});
+test.bind({ name: 2 });
 
 
 
 
-Function.prototype.myBind=function(target){
-	let context=target||window;
-	const _this=this;
-	let _args=[...arguments].splice(1);
-	return function(){
-		_this.apply(context,[...arguments,..._args,])
-	}
-}
